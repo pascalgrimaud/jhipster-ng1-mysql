fbf69fde9b5f7b7ea1622f4b1892980afa1d04ba
Pascal Grimaud <pascalgrimaud@gmail.com>
2017-07-31 07:49:55 +0200

    init project

---
+++ src/main/java/io/github/jhipster/travis/web/rest/util/HeaderUtil.java
@@ -0,0 +1,44 @@
+package io.github.jhipster.travis.web.rest.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+/**
+ * Utility class for HTTP headers creation.
+ */
+public final class HeaderUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(HeaderUtil.class);
+
+    private static final String APPLICATION_NAME = "travisMysqlApp";
+
+    private HeaderUtil() {
+    }
+
+    public static HttpHeaders createAlert(String message, String param) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-travisMysqlApp-alert", message);
+        headers.add("X-travisMysqlApp-params", param);
+        return headers;
+    }
+
+    public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".created", param);
+    }
+
+    public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".updated", param);
+    }
+
+    public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".deleted", param);
+    }
+
+    public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
+        log.error("Entity processing failed, {}", defaultMessage);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-travisMysqlApp-error", "error." + errorKey);
+        headers.add("X-travisMysqlApp-params", entityName);
+        return headers;
+    }
+}
